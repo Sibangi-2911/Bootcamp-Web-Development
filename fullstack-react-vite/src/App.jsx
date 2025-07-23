6bbe2ce663c6b05245a613f56b28579c884305b1
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-23 14:59:41 +0530

    values assigned to the properties for making iot dynamic

--- fullstack-react-vite/src/App.jsx
+++ fullstack-react-vite/src/App.jsx
@@ -1,11 +1,22 @@
 import UserProfile from "./components/userProfile/userProfile";
 
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  skills: "NodeJs, React",
+};
+const user1 = {
+  firstName: "Sibangi",
+  lastName: "Boxipatro",
+  skills: "HTML, React",
+};
+
 export default function App() {
 
   return (
     <>
-      <UserProfile />
-      <h1>Hello World</h1>
+      <UserProfile firstName={user.firstName}  lastName={user.lastName} skills={user.skills} />
+      <UserProfile firstName={user1.firstName} lastName={user1.lastName} skills={user1.skills} />
     </>
   )  
 }
