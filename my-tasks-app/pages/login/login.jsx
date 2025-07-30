f4d746b80520365214c85d2f0537b767ce73c598
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-30 22:31:13 +0530

    Login page function created

---
+++ my-tasks-app/pages/login/login.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+
+export default function Login(){
+  return(
+    <>
+      <h1>This is a Login Page.</h1>
+      <ul>
+        <li>
+          <Link to="tasks">Tasks</Link>
+        </li>
+        <li>
+          <Link to="signup">Signup</Link>
+        </li>
+      </ul>
+    </>
+  );
+}
