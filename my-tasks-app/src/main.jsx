b7e1837bb9866075046a5e85af6365c95865d613
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-30 22:27:32 +0530

    Simplified the code by importing the routes

--- my-tasks-app/src/main.jsx
+++ my-tasks-app/src/main.jsx
@@ -1,24 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Link, createBrowserRouter , RouterProvider} from 'react-router'
+import {  RouterProvider} from 'react-router'
 import './index.css'
-import App from './App.jsx'
+import { router } from "./routes.jsx";
 
-const router = createBrowserRouter([
-  {
-    path : "/",
-    element: (
-      <div>
-        <h1>Hello World!!</h1>
-        <Link to="login">Login</Link>
-      </div>
-    ),
-  },
-  {
-    path : "login",
-    element: <div>Login</div>,
-  }
-])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
