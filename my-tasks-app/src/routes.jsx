b645cc97efd58cc1db42f670242ca2c9bc1fe287
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-30 22:29:23 +0530

    All routes were added

---
+++ my-tasks-app/src/routes.jsx
@@ -0,0 +1,24 @@
+import Login from "../pages/login/login.jsx";
+import Signup  from "../pages/signup/signup.jsx";
+import Tasks from "../pages/tasks/tasks.jsx";
+import Error404 from "../pages/404Page/404.jsx";
+import  { createBrowserRouter } from 'react-router';
+
+export const router = createBrowserRouter([
+  {
+    path : "/",
+    element: <Login />,
+  },
+  {
+    path : "signup",
+    element: <Signup />,
+  },
+  {
+    path : "tasks",
+    element: <Tasks />,
+  },
+  {
+    path : "*",
+    element: <Error404 />,
+  },
+]);
