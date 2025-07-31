856c00f35ee1ef4ce7ef6247d6d59c939cd320ff
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-07-31 23:47:31 +0530

    login page layout completed

--- my-tasks-app/pages/login/login.jsx
+++ my-tasks-app/pages/login/login.jsx
@@ -1,20 +1,37 @@
 import { Link } from "react-router";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-export default function Login(){
-  return(
-    <>
-      <h1>This is a Login Page.</h1>
-      <ul>
-        <li>
-          <Link to="tasks">Tasks</Link>
-        </li>
-        <li>
-          <Button asChild>
-            <Link to="signup">Signup</Link>
-          </Button>
-        </li>
-      </ul>
-    </>
+
+export default function Login() {
+  return (
+    <section className="flex flex-row max-w-screen-xl min-h-screen w-full justify-center items-center">
+      <div className="w-4/12">
+        <Card>
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>
+              Login and Create Tasks
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Input className="mb-4" type="email" placeholder="Email" />
+            <Input className="mb-4" type="password" placeholder="Password" />
+          </CardContent>
+          <CardFooter className="flex flex-row justify-between">
+            <p className="basis-1/2">Don't have an account? {"  "} <Link to= "signup" className="hover:text-blue-500">Signup Here</Link></p>
+            <Button>Login</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </section>
   );
 }
