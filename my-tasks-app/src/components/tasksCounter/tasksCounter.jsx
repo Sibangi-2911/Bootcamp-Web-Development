43f6111938f2da8c1962f8a6ceb577c737f639f3
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-08-01 22:28:23 +0530

    defined the taskCounter function for the div element in the tasks page

---
+++ my-tasks-app/src/components/tasksCounter/tasksCounter.jsx
@@ -0,0 +1,10 @@
+export function TasksCounter() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="p-6 border-solid border-4 border-red-500 rounded-full mb-4">
+        <div className="min-w-10 min-h-10 text-white text-3xl text-center flex justify-center leading-10">12</div>
+      </div>
+      <div>Todo</div>
+    </div>
+  )
+}
