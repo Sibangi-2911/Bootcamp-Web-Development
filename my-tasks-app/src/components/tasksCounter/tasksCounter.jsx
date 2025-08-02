4c6becd19e5582f954a774e20330bce864b3f862
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-08-02 23:01:04 +0530

    props and tailwind css styles were added

--- my-tasks-app/src/components/tasksCounter/tasksCounter.jsx
+++ my-tasks-app/src/components/tasksCounter/tasksCounter.jsx
@@ -1,10 +1,18 @@
-export function TasksCounter() {
+export function TasksCounter({ type = "todo", count = 0 }) {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="p-6 border-solid border-4 border-red-500 rounded-full mb-4">
-        <div className="min-w-10 min-h-10 text-white text-3xl text-center flex justify-center leading-10">12</div>
+    <div className="flex flex-col items-center justify-center">
+      <div
+        className={`p-6 border-solid ${type === "todo" && "border-red-500"}  ${
+          type === "inProgress" && "border-orange-500"
+        }  ${
+          type === "completed" && "border-green-500"
+        } border-4 rounded-full mb-4`}
+      >
+        <div className="min-w-10 min-h-10 text-white text-3xl text-center flex justify-center leading-10">
+          {count}
+        </div>
       </div>
-      <div>Todo</div>
+      <div className="text-white text-xl text-center">{type}</div>
     </div>
-  )
+  );
 }
