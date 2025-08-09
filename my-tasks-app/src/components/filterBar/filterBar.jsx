2de612a4668d2cb1808436ca17d6657e1f854ce3
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-08-09 22:03:37 +0530

    modified the code for better layout

--- my-tasks-app/src/components/filterBar/filterBar.jsx
+++ my-tasks-app/src/components/filterBar/filterBar.jsx
@@ -1,13 +1,13 @@
-import { OrderSelect } from "../orderSelect/orderSelect"
-import { TaskPagination } from "../taskPagination/taskPagination"
+import { OrderSelect } from "../orderSelect/orderSelect";
+import { TaskPagination } from "../taskPagination/taskPagination";
 
-export function FilterBar(){
-  return(
+export function FilterBar() {
+  return (
     <>
-    <nav className="flex flex-row justify-between">
-      <TaskPagination />
-      <OrderSelect />
-    </nav>
+      <nav className="w-full flex flex-row justify-between items-center mt-6 px-2">
+        <TaskPagination />
+        <OrderSelect />
+      </nav>
     </>
-  )
+  );
 }
