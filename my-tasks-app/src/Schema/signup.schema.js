51c4136e2ec36f24fb251e85bfd5047fda718968
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-08-16 23:18:11 +0530

    signup schema modified

--- my-tasks-app/src/Schema/signup.schema.js
+++ my-tasks-app/src/Schema/signup.schema.js
@@ -1,24 +1,23 @@
 import { z } from "zod";
 
-const passwordValidation = new RegExp(
-  /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
-);
+const passwordValidation =
+  /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
 export const SignupSchema = z.object({
   firstName: z
     .string()
-    .min(3, {
-      message: "First Name must be at least 3 characters.",
-    })
-    .max(100, {
-      message: "First name cannot be more than 100 characters.",
-    }),
-  lastName: z.string().optional().max(100, {
-    message: "Last name cannot be more than 100 characters.",
-  }),
-  email: z.email().string(),
+    .min(3, { message: "First Name must be at least 3 characters." })
+    .max(100, { message: "First name cannot be more than 100 characters." }),
+
+  lastName: z
+    .string()
+    .max(100, { message: "Last name cannot be more than 100 characters." })
+    .optional(),
+
+  email: z.string().email({ message: "Please enter a valid email address." }),
+
   password: z.string().regex(passwordValidation, {
     message:
-      "Password must include at least one number, one uppercase letter, one lowercase letter, and one special character.",
+      "Password must be at least 8 characters and include at least one number, one uppercase letter, one lowercase letter, and one special character.",
   }),
 });
