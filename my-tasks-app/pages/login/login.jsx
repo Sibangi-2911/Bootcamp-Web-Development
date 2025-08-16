af43f1cb834c4687a8fa61a179043a1b9c2c3d7a
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-08-16 23:31:33 +0530

    zodresolver and form component were added

--- my-tasks-app/pages/login/login.jsx
+++ my-tasks-app/pages/login/login.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router";
 import {
   Card,
   CardContent,
@@ -7,29 +6,81 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { LoginSchema } from "@/Schema/login.schema";
 
 export default function Login() {
+  const form = useForm({
+    resolver: zodResolver(LoginSchema),
+  });
+  function onSubmit(values) {
+    console.log(values);
+  }
   return (
     <section className="flex flex-row max-w-screen-xl min-h-screen w-full justify-center items-center">
       <div className="w-4/12">
         <Card>
           <CardHeader>
             <CardTitle>Login</CardTitle>
-            <CardDescription>
-              Login and Create Tasks
-            </CardDescription>
+            <CardDescription>Login and Create Tasks</CardDescription>
           </CardHeader>
-          <CardContent>
-            <Input className="mb-4" type="email" placeholder="Email" />
-            <Input className="mb-4" type="password" placeholder="Password" />
-          </CardContent>
-          <CardFooter className="flex flex-row justify-between">
-            <p className="basis-1/2">Don't have an account? {"  "} <Link to= "signup" className="hover:text-blue-500">Signup Here</Link></p>
-            <Button>Login</Button>
-          </CardFooter>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <CardContent>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input placeholder="Email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="Password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </CardContent>
+              <CardFooter className="flex flex-row justify-between">
+                <p className="basis-1/2">
+                  Don't have an account? {"  "}{" "}
+                  <Link to="signup" className="hover:text-blue-500">
+                    Signup Here
+                  </Link>
+                </p>
+                <Button type="submit">Login</Button>
+              </CardFooter>
+            </form>
+          </Form>
         </Card>
       </div>
     </section>
